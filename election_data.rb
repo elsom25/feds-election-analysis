c70c2a3f0c9347a489d093d51a7321c87b23b25b
Jesse McGinnis <jessieelsom@hotmail.com>
2013-10-23 22:27:28 -0400

    always dealing with lists of files.

--- election_data.rb
+++ election_data.rb
@@ -13,24 +13,66 @@ Bundler.require :default
 #
 # Initial Usage:
 #
-# 	election_data = ElectionData.new({ voter_list: CSV_FILE_ARR, results: FILE })
+# 	election_data = ElectionData.new( CSV_FILE_ARR, TXT_FILE_ARR )
 # 	table = election_data.create_table
 #
 # 	...or...
 #
 # 	election_data = InvariantPairTable.new
-# 	table = election_data.create_table({ voter_list: CSV_FILE_ARR, results: FILE })
+# 	table = election_data.create_table( CSV_FILE_ARR, TXT_FILE_ARR )
 class ElectionData
 
-	def initialize(params = nil)
-    setup(params) if params
+	def initialize(voters_list=nil, results_list=nil)
+    setup(voters_list, results_list) if voters_list && results_list
   end
 
-  def create_lookup(params = nil)
-    setup(params) if params
-    raise 'You must provide both a `voter_list` and `results`.' unless @voter_list_files && @results_file
-    setup_table
+  def create_lookup(voters_list=nil, results_list=nil)
+    setup(voters_list, results_list) if voters_list && results_list
+    raise 'You must provide `voter_list`.' unless @voter_list_files
+    raise 'You must provide `results_list`.' unless @results_list_file
 
+    build_table
+    import_voter_lists
+
+
+    pp @results_list_file
+    # pp open(@results, &:read)
+
+    # pp @election_data.all
+
+  end
+
+protected
+
+  def setup(voters_list, results_list)
+    raise 'voters_list must be an Array.' unless voters_list.is_a? Array
+    raise 'results_list must be an Array.' unless results_list.is_a? Array
+
+    @voter_list_files  = voters_list
+    @results_list_file = results_list
+  end
+
+  def build_table
+    @@DB = Sequel.sqlite
+    @@DB.create_table :election_data do
+      primary_key :id
+      Integer :student_id, unique: true
+      String  :user_id, unique: true
+
+      String  :first_name
+      String  :middle_name
+      String  :last_name
+      String  :email
+
+      String  :program
+      String  :academic_plan
+      String  :term
+      String  :campus
+    end
+    @election_data = @@DB[:election_data]
+  end
+
+  def import_voter_lists
     # need to nuke the first line, since it interfears with headers
     @voter_list_files = @voter_list_files.map do |input|
       output = "__TEMP__#{input}"
@@ -38,7 +80,6 @@ class ElectionData
       output
     end
 
-    @election_data = @@DB[:election_data]
     @voter_list_files.each do |f|
       CSV.foreach(f, headers: :first_row) do |row|
         @election_data.insert(
@@ -56,37 +97,6 @@ class ElectionData
       end
     end
     FileUtils.rm @voter_list_files
-
-    pp @election_data.all
-
-  end
-
-protected
-
-  def setup(params)
-    raise 'Params must be a Hash.' unless params.is_a? Hash
-
-    @voter_list_files = params[:voter_list]
-    @results_file     = params[:results]
-  end
-
-  def setup_table
-    @@DB = Sequel.sqlite
-    @@DB.create_table :election_data do
-      primary_key :id
-      Integer :student_id, unique: true
-      String  :user_id, unique: true
-
-      String  :first_name
-      String  :middle_name
-      String  :last_name
-      String  :email
-
-      String  :program
-      String  :academic_plan
-      String  :term
-      String  :campus
-    end
   end
 
 end
